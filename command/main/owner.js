ebebdbf221f07bca19970469e666390cfd606f0f
KriZ SeR <92368721+KANNANSIR@users.noreply.github.com>
2022-06-29 08:12:53 +0530

    Update owner.js

--- command/main/owner.js
+++ command/main/owner.js
@@ -6,15 +6,15 @@ module.exports = {
    async exec(msg, sock, args, arg) {
       const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:RzxGamz\n' // full name
-            + 'ORG:Programmer;\n' // the organization of the contact
-            + 'TEL;type=CELL;type=VOICE;waid=1 551 786 8060:+1 551 786 8060\n' // WhatsApp ID + phone number
+            + 'FN:𝖐𝖗𝖎𝖘𝖍𝖓𝖆𝖉𝖆𝖘\n' // full name
+            + 'ORG:𝔦𝔫𝔡𝔦𝔞;\n' // the organization of the contact
+            + 'TEL;type=CELL;type=VOICE;waid=919633687665:919633687665\n' // WhatsApp ID + phone number
             + 'END:VCARD'
       sock.sendMessage(
       msg.from,
         { 
            contacts: { 
-            displayName: 'Jeff', 
+            displayName: '𝔨𝔯𝔦𝔷 𝔰𝔢𝔯', 
             contacts: [{ vcard }] 
            }
         }, { quoted: msg }
